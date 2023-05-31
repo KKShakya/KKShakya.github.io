4f3bc42c16ae187145c4ee5aa3aeddb354294ec1
Krishna Kumar Shakya <Krishnashakya7272@gmail.com>
2023-05-31 10:36:50 +0530

    Project typing added

--- src/components/data/ProjectData.js
+++ src/components/data/ProjectData.js
@@ -1,124 +1,152 @@
-import bluemercury from '../Images/BlueMercury.png' 
-import beyoung from '../Images/BeYoung.png' 
-import WakeHealhty from '../Images/WakeHealthy.png' 
+import bluemercury from '../Images/BlueMercury.png'
+import beyoung from '../Images/BeYoung.png'
+import WakeHealhty from '../Images/WakeHealthy.png'
 import ESPN from '../Images/ESPN.png'
+import TYPING from '../Images/Typing-tutor.png'
 export const ProjectData = [
- 
-  {
-      "name": "BlueMercury",
-      "desc": "An ecommerce website clone named as- Bluemercy, a cosmetic shopping website. You can Browse all the products you want and put them in cart if you like . Sort them , filter them and choose form your range. Add to cart and checkout. ",
-      "img": bluemercury,
-      "git": "https://github.com/KKShakya/BlueMercury",
-      "deploy": "https://bluemercury-tawny.vercel.app/",
-      "techStack":[
-          {
-              "name": "HTML",
-              "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
-          },
-          {
-              "name": "CSS",
-              "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
-          },
-          {
-              "name": "JavaScript",
-              "img":  "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-          
-          },
-          
-      ]
-  },
- 
-  {
-      "name": "Wake Healthy",
-      "desc": "A Platform for virtual training programs available for your special workouts starting from beginner level to Pro. Also different offline Gym centers available with great pricing to join . Don't just wait there start your journey with wake healthy now. ",
-      "img": WakeHealhty,
-      "git": "https://github.com/KKShakya/cruel-passenger-3092",
-      "deploy": "https://wakehealthy.vercel.app/",
-      "techStack":[
-          {
-              "name": "HTML",
-              "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
-          },
-          {
-              "name": "CSS",
-              "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
-          },
-          {
-              "name": "JavaScript",
-              "img":  "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-          
-          },
-          {
-            "name": "React",
-            "img": "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
-        },
-        {
-            "name": "NodeJs",
-            "img": "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
-        },
-        {
-            "name": "Chakra UI",
-            "img":  "https://camo.githubusercontent.com/eec63163a5209f78e8b0e3d6ab82eb981d03c4453a7a466f4fdc063da09f2a4d/68747470733a2f2f692e696d6775722e636f6d2f454d796b5a62332e706e67"
-        },
-      ]
-  },
-  {
-    "name": "BeYoung",
-    "desc": "This is an Ecommerce website made with ReactJS and Chakra UI . I have used Myntra API here for the products sections. You can have different sections Kids, women's and men's .You can shop around for your choice.",
-    "img": beyoung,
-    "git": "https://github.com/KKShakya/BeYoung",
-    "deploy": "https://6370f66c528c784a0d465a3d--beyoungcontructweek.netlify.app/",
-    "techStack":[
-        {
-            "name": "React",
-            "img": "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
-        },
-        {
-            "name": "NodeJs",
-            "img": "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
-        },
-        {
-            "name": "JavaScript",
-            "img":  "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-        
-        },
-        {
-            "name": "Chakra UI",
-            "img":  "https://camo.githubusercontent.com/eec63163a5209f78e8b0e3d6ab82eb981d03c4453a7a466f4fdc063da09f2a4d/68747470733a2f2f692e696d6775722e636f6d2f454d796b5a62332e706e67"
-        },
-        
-    ]
-},
-  {
-      "name": "ESPN IPL Automation",
-      "desc": "This repository deals with usage of puppeteer automation using promises in Javascript. You can just start the project on chromium and can see the magic of Puppeteer . How it automatically visits the site and fetches the desired data.",
-      "img": ESPN,
-      "git": "https://github.com/KKShakya/espn_ipl_automation",
-      "deploy": "https://drive.google.com/file/d/1METDO6RXiEsBVOT94f30dkoWUSN9H0gZ/view?usp=sharing",
-      "techStack":[
-          {
-              "name": "HTML",
-              "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
-          },
-          {
-              "name": "CSS",
-              "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
-          },
-          {
-              "name": "JavaScript",
-              "img":  "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
-          
-          },
-      
-        {
-            "name": "NodeJs",
-            "img": "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
-        },
-        {
-            "name": "Puppeteer",
-            "img":  "https://user-images.githubusercontent.com/10379601/29446482-04f7036a-841f-11e7-9872-91d1fc2ea683.png"
-        },
-      ]
-  },
- 
+
+
+    //Wake Healthy
+    {
+        "name": "Wake Healthy",
+        "desc": "A Platform for virtual training programs available for your special workouts starting from beginner level to Pro. Also different offline Gym centers available with great pricing to join . Don't just wait there start your journey with wake healthy now. ",
+        "img": WakeHealhty,
+        "git": "https://github.com/KKShakya/cruel-passenger-3092",
+        "deploy": "https://wakehealthy.vercel.app/",
+        "techStack": [
+            {
+                "name": "HTML",
+                "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
+            },
+            {
+                "name": "CSS",
+                "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
+            },
+            {
+                "name": "JavaScript",
+                "img": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+
+            },
+            {
+                "name": "React",
+                "img": "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+            },
+            {
+                "name": "NodeJs",
+                "img": "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
+            },
+            {
+                "name": "Chakra UI",
+                "img": "https://camo.githubusercontent.com/eec63163a5209f78e8b0e3d6ab82eb981d03c4453a7a466f4fdc063da09f2a4d/68747470733a2f2f692e696d6775722e636f6d2f454d796b5a62332e706e67"
+            },
+
+            {
+                "name": "Redux",
+                "img": "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png"
+            },
+        ]
+    },
+
+    // Minit-Typing-tutor
+    {
+        "name": "Mini-Typing-Tutor",
+        "desc": "This Project is the mini version of typing tutor allowing to check your accuracy and performance with keys [a,s,d,f,j,k,l]. The sounds are provided for wrong key, clicks, and session end for the user experience.",
+        "img": TYPING,
+        "git": "https://github.com/KKShakya/Mini-typing-tutor",
+        "deploy": "https://typingtutor-kkshakya.vercel.app/",
+        "techStack": [
+            {
+                "name": "HTML",
+                "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
+            },
+            {
+                "name": "CSS",
+                "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
+            },
+            {
+                "name": "JavaScript",
+                "img": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+
+            },
+
+            {
+                "name": "NodeJs",
+                "img": "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
+            },
+
+            {
+                "name": "React",
+                "img": "https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+            },
+
+            {
+                "name": "Redux",
+                "img": "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png"
+            },
+
+        ]
+    },
+
+
+    //Blue Mercury
+    {
+        "name": "BlueMercury",
+        "desc": "An ecommerce website clone named as- Bluemercy, a cosmetic shopping website. You can Browse all the products you want and put them in cart if you like . Sort them , filter them and choose form your range. Add to cart and checkout. ",
+        "img": bluemercury,
+        "git": "https://github.com/KKShakya/BlueMercury",
+        "deploy": "https://bluemercury-tawny.vercel.app/",
+        "techStack": [
+            {
+                "name": "HTML",
+                "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
+            },
+            {
+                "name": "CSS",
+                "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
+            },
+            {
+                "name": "JavaScript",
+                "img": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+
+            },
+
+        ]
+    },
+
+
+    //ESPN Ipl Automation
+    {
+        "name": "ESPN IPL Automation",
+        "desc": "This repository deals with usage of puppeteer automation using promises in Javascript. You can just start the project on chromium and can see the magic of Puppeteer . How it automatically visits the site and fetches the desired data.",
+        "img": ESPN,
+        "git": "https://github.com/KKShakya/espn_ipl_automation",
+        "deploy": "https://drive.google.com/file/d/1METDO6RXiEsBVOT94f30dkoWUSN9H0gZ/view?usp=sharing",
+        "techStack": [
+            {
+                "name": "HTML",
+                "img": "https://cdn-icons-png.flaticon.com/512/1532/1532556.png"
+            },
+            {
+                "name": "CSS",
+                "img": "https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png"
+            },
+            {
+                "name": "JavaScript",
+                "img": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+
+            },
+
+            {
+                "name": "NodeJs",
+                "img": "https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png"
+            },
+            {
+                "name": "Puppeteer",
+                "img": "https://user-images.githubusercontent.com/10379601/29446482-04f7036a-841f-11e7-9872-91d1fc2ea683.png"
+            },
+        ]
+    },
+
+
+
 ]
